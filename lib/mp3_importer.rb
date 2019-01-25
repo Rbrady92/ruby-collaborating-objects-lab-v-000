b04d7fc0d6e308e219c9887191639f3020f3b507
Ryan Brady <ryanbrady369@gmail.com>
2019-01-25 22:08:50 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,17 @@
-files = Files.read(./spec/fixtures)
-files = files.split("\n")
-Mp3Importer.new(files)   #could prob split here 
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
+
+end
